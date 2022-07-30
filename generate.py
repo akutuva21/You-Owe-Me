7e10975fae88e3191e9799388b9c21083f6e5c21
Prayuj Tuli <82406637+prayujt@users.noreply.github.com>
2022-07-30 19:57:33 -0400

    Added giant transactions file

--- generate.py
+++ generate.py
@@ -1,2 +1,23 @@
-generatedFile = open('./hugetransactions.txt')
+import names
+import random
 
+generatedFile = open('./hugetransactions.txt', 'w')
+
+names_list = []
+
+def add():
+    name = names.get_first_name()
+    if name not in names_list:
+        names_list.append(names.get_first_name())
+    else:
+        add()
+
+for i in range(0, 1000):
+    add()
+
+for i in range(0, 100000):
+    value = str(round(random.uniform(1, 100), 2))
+    randomPerson1 = names_list[random.randint(0, 999)]
+    randomPerson2 = names_list[random.randint(0, 999)]
+
+    generatedFile.write(randomPerson1 + ' ' + randomPerson2 + ' ' + value + '\n')
